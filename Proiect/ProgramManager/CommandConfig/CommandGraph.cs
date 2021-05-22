0ecf58dd31cfb4fb21be52cdf3f291ddceaa89c5
Batalan Vlad <batabtro@gmail.com>
2021-05-22 17:13:58 +0300

    Finally managed comments

--- Proiect/ProgramManager/CommandConfig/CommandGraph.cs
+++ Proiect/ProgramManager/CommandConfig/CommandGraph.cs
@@ -1,20 +1,61 @@
-﻿
+﻿/**************************************************************************
+ *                                                                        *
+ *  File:        CommandGraph.cs                                          *
+ *  Copyright:   (c) 2021, Batalan Vlad                                   *
+ *  E-mail:      vlad.batalan@student.tuiasi.ro                           *
+ *  Website:     https://github.com/vladbatalan/LogicallyInterpretor      *
+ *  Description:  A graph full of commands ready to be used.              *
+ *                                                                        *
+ *  This code and information is provided "as is" without warranty of     *
+ *  any kind, either expressed or implied, including but not limited      *
+ *  to the implied warranties of merchantability or fitness for a         *
+ *  particular purpose. You are free to use this source code in your      *
+ *  applications as long as the original copyright notice is included.    *
+ *                                                                        *
+ **************************************************************************/
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace LogicalSchemeManager
 {
+    /// <summary>
+    /// The class describes a graph full of different commands
+    /// </summary>
     class CommandGraph : ICommandConfiguration
     {
+        #region Fields
+        /// <summary>
+        /// A graph which contains a dictionary of different elements
+        /// </summary>
         private Dictionary<ICommand, ICommand[]> _graph;
-        private ICommand _startPoint;
-        private ICommand _endPoint;
 
+        /// <summary>
+        /// The starting point of the element
+        /// </summary>
+        private ICommand _startPoint;
+
+        /// <summary>
+        /// The final point of the element
+        /// </summary>
+        private ICommand _endPoint;
+        #endregion Fields
+
+        #region Constructors
+        /// <summary>
+        /// The implicit constructor
+        /// </summary>
         public CommandGraph()
         {
             _graph = new Dictionary<ICommand, ICommand[]>();
         }
+
+        /// <summary>
+        /// The parameter constructor
+        /// </summary>
+        /// <param name="start">The starting point</param>
+        /// <param name="end">The ending point</param>
         public CommandGraph(ICommand start, ICommand end)
         {
             _startPoint = start;
@@ -23,23 +64,42 @@ namespace LogicalSchemeManager
             AddElement(_startPoint);
             AddElement(_endPoint);
         }
+        #endregion Constructors
 
-        public Dictionary<ICommand, ICommand[]> Graph {
+        #region Properties
+        /// <summary>
+        /// The property that exposes the dictionary
+        /// </summary>
+        public Dictionary<ICommand, ICommand[]> Graph
+        {
             get => _graph;
         }
-        public List<ICommand> CommandList {
-            get {
+
+        /// <summary>
+        /// The property that exposes the list of commands
+        /// </summary>
+        public List<ICommand> CommandList
+        {
+            get
+            {
                 return _graph.Keys.ToList<ICommand>();
             }
         }
 
-        public ICommand StartPoint {
+        /// <summary>
+        /// The property that exposes the starting point
+        /// </summary>
+        public ICommand StartPoint
+        {
             get
             {
                 return _startPoint;
             }
         }
 
+        /// <summary>
+        /// The property that exposes the ending point
+        /// </summary>
         public ICommand EndPoint
         {
             get
@@ -47,7 +107,12 @@ namespace LogicalSchemeManager
                 return _endPoint;
             }
         }
+        #endregion Properties
 
+        #region Methods
+        /// <summary>
+        /// The method that adds an element
+        /// </summary>
         public void AddElement(ICommand command)
         {
             if (!_graph.ContainsKey(command))
@@ -56,6 +121,9 @@ namespace LogicalSchemeManager
             }
         }
 
+        /// <summary>
+        /// The method that bind the first element
+        /// </summary>
         public void BindElementFirst(ICommand father, ICommand firstSon)
         {
             if (_graph.ContainsKey(father))
@@ -68,6 +136,9 @@ namespace LogicalSchemeManager
             }
         }
 
+        /// <summary>
+        /// The method that bind the second element
+        /// </summary>
         public void BindElementSecond(ICommand father, ICommand secondSon)
         {
             if (_graph.ContainsKey(father))
@@ -80,17 +151,25 @@ namespace LogicalSchemeManager
             }
         }
 
+        /// <summary>
+        /// The method that removes an output
+        /// </summary>
         public void RemoveElement(ICommand command)
         {
-            if(_graph.ContainsKey(command))
+            if (_graph.ContainsKey(command))
                 _graph.Remove(command);
         }
 
+        /// <summary>
+        /// The abstract method returns the branch towards the next command
+        /// </summary>
+        /// <returns>A command that describes the branch taken to the next element to be executed</returns>
         public ICommand GetNextElement(ICommand key, bool isNextTrue)
         {
             if (isNextTrue) return _graph[key][0];
             else return _graph[key][1];
         }
+        #endregion Methods
 
     }
 }
